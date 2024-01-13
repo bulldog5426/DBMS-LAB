cfad2bf0bbe94d41e63259ed7234360d99a48e74
Katherine Pierce <141926358+bulldog5426@users.noreply.github.com>
2024-01-13 16:29:32 +0530

    Add files via upload

---
+++ lab1/lab1/student/student/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student
+{
+    public partial class Form1 : Form
+    {
+        string name = string.Empty;
+        string branch = string.Empty;
+        string gender = string.Empty;
+        string date = string.Empty;
+        string amenities = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            branch = this.comboBox1.SelectedText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            name = this.textBox1.Text;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked)
+            {
+                gender = " male ";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton2.Checked)
+            {
+                gender = " female";
+            }
+        }
+    }
+}
